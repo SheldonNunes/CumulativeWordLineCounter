7e219f55158fe1271d752f993f083b15313bda6a
Sheldon Nunes <sheldon.nunes@quoralis.com>
2019-02-17 12:48:05 +1300

    Change adds initial word line converter

--- src/App.js
+++ src/App.js
@@ -3,23 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: '', numberOfWords: 0};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  
+  handleChange(event) {
+      let wordCountPerRow = event.target.value.split('\n').map(function(value, index) {
+        return value.trim().split(/\b(\s)/).reduce(function(acc, value) {
+          if(value != ' ' && value != '') {
+            return acc + 1;
+          } else {
+            return acc;
+          }
+        }, 0);
+      });
+
+      let accumulated = 0;
+      let accumulatedWordCountPerRow = [];
+      wordCountPerRow.forEach(element => {
+        accumulatedWordCountPerRow.push(accumulated += element);
+      });
+
+      let stringRepr = accumulatedWordCountPerRow.reduce(function(acc, value){
+        return acc + value + '\n';
+      }, '')
+
+      this.setState(
+      {
+          text: event.target.value, 
+          wordCountPerRow: wordCountPerRow,
+          accumulatedWordCountPerRow: accumulatedWordCountPerRow,
+          stringRepr: stringRepr
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <textarea className="textarea" rows='50' placeholder="Paste your lines of text here:" value={this.state.text}  onChange={this.handleChange}/>
+          <textarea className="textarea" rows='50' value={this.state.stringRepr} readOnly/>
+
+        </div>
       </div>
     );
   }
